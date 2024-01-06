ca2990bebc60b21a3d4cb68385006f5cb7478b1e
Lucian <63058063+Catalin-Lucian@users.noreply.github.com>
2024-01-06 17:22:34 +0200

    added defocused

--- RayTracingCPU/RayTracingCPU/utils.h
+++ RayTracingCPU/RayTracingCPU/utils.h
@@ -17,7 +17,6 @@ using std::sqrt;
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
-// Utility Functions
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
